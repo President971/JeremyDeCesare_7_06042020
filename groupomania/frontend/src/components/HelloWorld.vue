49372e66951f66e1d2028b19b68966530e80635b
Jeremy De Cesare <decesare971@gmail.com>
2021-10-08 18:03:37 +0200

    Indexation

---
+++ groupomania/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="480"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Inscription
+        </v-card-title>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+  </v-form>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Inscription
+          </v-btn> 
+          <v-btn
+            color="green darken-1"
+            text
+            @click="dialog = false"
+          >
+            Connexion
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      dialog: true,
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ]
+    }),
+  }
+</script>
