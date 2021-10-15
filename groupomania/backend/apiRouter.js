259aea26b3a8f93700a58b7f0d147916db194d15
Jeremy De Cesare <decesare971@gmail.com>
2021-10-15 16:18:59 +0200

    Fix requete CORS

--- groupomania/backend/apiRouter.js
+++ groupomania/backend/apiRouter.js
@@ -8,6 +8,13 @@ var likesCtrl    = require('./routes/likesCtrl');
 exports.router = (function() {
   var apiRouter = express.Router();
 
+  apiRouter.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
   // Users routes
   apiRouter.route('/users/register/').post(usersCtrl.register);
   apiRouter.route('/users/login/').post(usersCtrl.login);
