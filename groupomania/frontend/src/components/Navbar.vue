e086ffb9143f65aae0ad5013cde04867ce05beb2
Jeremy De Cesare <decesare971@gmail.com>
2021-10-16 17:40:58 +0200

    signout implenté

--- groupomania/frontend/src/components/Navbar.vue
+++ groupomania/frontend/src/components/Navbar.vue
@@ -1,28 +1,34 @@
 <template>
-<div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">Groupomania</b-navbar-brand>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Groupomania</b-navbar-brand>
 
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>Utilisateur</em>
-          </template>
-          <router-link to="/profil">Profil</router-link>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>Utilisateur</em>
+            </template>
+            <router-link to="/profil">Profil</router-link>
+            <b-dropdown-item @click="logout()">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Navbar"
-}
+  name: "Navbar",
+  methods: {
+    logout: function () {
+      this.$store.commit("logout");
+      this.$router.push("/");
+    },
+  },
+};
 </script>
--- groupomania/frontend/src/components/Profile.vue
+++ groupomania/frontend/src/components/Profile.vue
@@ -4,11 +4,6 @@
     <p class="card__subtitle">Voilà donc qui je suis...</p>
     <p>{{user.prenom}} {{user.nom}} {{user.email}}</p>
     <img :src="user.photo"/>
-    <div class="form-row">
-      <button @click="logout()" class="button">
-        Déconnexion
-      </button>
-    </div>
   </div>
 </template>
 
