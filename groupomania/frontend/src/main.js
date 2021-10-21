97a7b7aa55c4466335c1e9ce8d9c11ce3c799191
Jeremy De Cesare <decesare971@gmail.com>
2021-10-21 23:05:33 +0200

    ok

--- groupomania/frontend/src/main.js
+++ groupomania/frontend/src/main.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 
 
 Vue.config.productionTip = false
