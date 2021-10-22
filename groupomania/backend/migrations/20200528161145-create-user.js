1db08fd32fc610c23910252c11a9a25e74d6cde7
Jeremy De Cesare <decesare971@gmail.com>
2021-10-22 20:51:49 +0200

    test post

--- groupomania/backend/migrations/20200528161145-create-user.js
+++ groupomania/backend/migrations/20200528161145-create-user.js
@@ -20,10 +20,6 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      avatar: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
       isAdmin: {
         allowNull: false,
         type: Sequelize.BOOLEAN
