96d0de80ecbbdae87b293bfcbf1fc8635a401c26
Jeremy De Cesare <decesare971@gmail.com>
2021-10-26 17:25:10 +0200

    integration twitter card

--- groupomania/frontend/src/components/Answer.vue
+++ groupomania/frontend/src/components/Answer.vue
@@ -1,46 +1,49 @@
 <template>
-  <b-container fluid>
-    <b-card no-body class="overflow-hidden mx-auto bg" style="max-width: 760px">
-      <b-row no-gutters>
-        <b-col>
-          <b-card
-            v-for="answer in allAnswers"
-            :key="answer.postid"
-            no-body
-            class="mx-auto bg mb-4"
-            style="max-width: 760px"
-          >
-            <b-card-text>
-              <p>{{ answer.content }}</p>
-            </b-card-text>
-          </b-card>
-          <b-card-body>
-            <div class="mb-4">
-              <h5>Mettre un commentaire</h5>
-            </div>
-            <b-row>
-              <b-form-textarea
-                id="input_text"
-                v-model="content"
-                placeholder="Ajouter un texte"
-                rows="2"
-                max-rows="3"
-              ></b-form-textarea>
-            </b-row>
-            <b-row>
-              <!-- Styled -->
-              <b-col>
-                <b-button @click="newAnswer()" variant="danger" class="mt-4">
-                  Poster
-                </b-button>
-              </b-col>
-            </b-row>
-          </b-card-body>
-        </b-col>
-      </b-row>
-    </b-card>
+  <b-container fluid no-body>
+    <b-row no-body class="overflow-hidden mx-auto bg" style="max-width: 760px">
+      <v-card
+        v-for="answer in allAnswers"
+        :key="answer.postid"
+        class="mx-auto bg mb-4"
+        color="red darken-1"
+        dark
+        max-width="760"
+      >
+        <v-card-title>
+          <v-icon large left>
+            mdi-account
+          </v-icon>
+          <span class="text-h6 font-weight-light"> {{ user.username }} </span>
+        </v-card-title>
+
+        <v-card-text class="text-h5 font-weight-bold">
+          {{ answer.content }}
+        </v-card-text>
+      </v-card>
+      <b-card-body>
+        <div class="mb-4">
+          <h5>Mettre un commentaire</h5>
+        </div>
+        <b-row>
+          <b-form-textarea
+            id="input_text"
+            v-model="content"
+            placeholder="Ajouter un texte"
+            rows="2"
+            max-rows="3"
+          ></b-form-textarea>
+        </b-row>
+        <b-row>
+          <!-- Styled -->
+          <b-col>
+            <b-button @click="newAnswer()" variant="danger" class="mt-4">
+              Poster
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-card-body>
+    </b-row>
   </b-container>
-  
 </template>
 
 <script>
